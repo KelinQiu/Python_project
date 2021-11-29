6640687a3eafa0212ef6026a182eb64daf507248
remete1 <remete1@163.com>
2021-11-30 00:53:00 +0800

    modified some errors in the broken_score code

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,3 +1,8 @@
+"""
+CP1404 - Practical 3
+Broken program to determine score status
+"""
+
 def main():
     score=float(input("Enter score: "))
     print(determine_score(score))
@@ -5,10 +10,19 @@ def main():
 def determine_score(score):
     if score<0 or score>100:
         return "Invalid score"
-    elif score>=90:
-        return "Excellent"
+    elif score>=85:
+        return "High Distinction"
+    elif score>=75:
+        return "Distinction"
+    elif score>=65:
+        return "Credit"
     elif score>=50:
-        return "Pass"
+        return "P"
     else:
-        return "Bad"
+        return "Fail"
 main()
+
+import random
+random_score = random.randint(0, 100)
+print(f"Score: {random_score}")
+print(determine_score(random_score))
