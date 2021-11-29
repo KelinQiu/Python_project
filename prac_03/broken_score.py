6c5ba7ca0405f441c30d4418789782d92163b54d
remete1 <remete1@163.com>
2021-11-30 00:48:52 +0800

    Revert "modified some errors in the password_check code"
    This reverts commit 2e44462cc17227e6a25bd568cd798ff59c17ff56.

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,8 +1,3 @@
-"""
-CP1404 - Practical 3
-Broken program to determine score status
-"""
-
 def main():
     score=float(input("Enter score: "))
     print(determine_score(score))
@@ -10,21 +5,10 @@ def main():
 def determine_score(score):
     if score<0 or score>100:
         return "Invalid score"
-    elif score>=85:
-        return "High Distinction"
-    elif score>=75:
-        return "Distinction"
-    elif score>=65:
-        return "Credit"
+    elif score>=90:
+        return "Excellent"
     elif score>=50:
-        return "P"
+        return "Pass"
     else:
-        return "Fail"
+        return "Bad"
 main()
-
-import random
-random_score = random.randint(0, 100)
-print(f"Score: {random_score}")
-print(determine_score(random_score))
-
-
