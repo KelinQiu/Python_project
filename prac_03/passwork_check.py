39fa96b65c21aaceeba5d591330b11221bf45aae
remete1 <remete1@163.com>
2021-11-30 00:58:13 +0800

    modified some errors in the password check use functions.

--- prac_03/passwork_check.py
+++ prac_03/passwork_check.py
@@ -1,17 +1,33 @@
-PASSWORD_MINIMUM_LENGTH=4
+'''
+CP1404 - Practical 3
+Password Check
+'''
+
+PASSWORD_MINIMUM_LENGTH=8
+PASSWORD_MAXIMUM_LENGTH=16
+
+def version_1():
+    password=input("Enter a password between {} and {} characters: ".format(PASSWORD_MINIMUM_LENGTH,PASSWORD_MAXIMUM_LENGTH))
+    while len(password) < PASSWORD_MINIMUM_LENGTH:
+        print("Password is too short.")
+        while len(password) > PASSWORD_MAXIMUM_LENGTH:
+            print("Password is too long.")
+    print('*'*len(count))
 
 def main():
-    password=get_password(PASSWORD_MINIMUM_LENGTH)
+    password=get_password(PASSWORD_MINIMUM_LENGTH,PASSWORD_MAXIMUM_LENGTH)
     print_asterisks(password)
 
-def get_password(minimum_length):
-    password=input("Enter password of at least{} characters: ".format(minimum_length))
-    while len(password) < minimum_length:
-        print("Password is too short")
-        password=input("Enter password of at least {} characters: ".format(minimum_length))
+def get_password(PASSWORD_MINIMUM_LENGTH,PASSWORD_MAXIMUM_LENGTH):
+    password=input("Enter a password between {} and {} characters: ".format(PASSWORD_MINIMUM_LENGTH,PASSWORD_MAXIMUM_LENGTH))
+    while len(password) < PASSWORD_MINIMUM_LENGTH:
+        print("Password is too short.")
+        while len(password) > PASSWORD_MAXIMUM_LENGTH:
+            print("Password is too long.")
+        password=input("Enter password between{} and {} characters: ".format(PASSWORD_MINIMUM_LENGTH,PASSWORD_MAXIMUM_LENGTH))
     return password
 
-def print_asterisks(sequence):
-    print('*'*len(sequence))
+def print_asterisks(count):
+    print('*'*len(count))
 
 main()
